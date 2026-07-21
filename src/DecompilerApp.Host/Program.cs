2817c06917022567066cbeb69f3cfe16de7c95ed
Archangel <jesse@archangel.wtf>
2026-07-22 01:38:20 +0200

    Put program in a STAThread

--- src/DecompilerApp.Host/Program.cs
+++ src/DecompilerApp.Host/Program.cs
@@ -7,15 +7,23 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Photino.Blazor;
 
-var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
-builder.Services.AddLogging(logging => logging.AddConsole());
-builder.Services.AddSingleton<IDecompilerBackend, DecompilerBackend>();
-builder.Services.AddSingleton<IProjectExportService, ProjectExportService>();
-builder.Services.AddSingleton<WorkspaceState>();
-builder.Services.AddSingleton<IFileDialogService, PhotinoFileDialogService>();
-builder.Services.AddSingleton<IWorkspaceSessionService, WorkspaceSessionService>();
-builder.RootComponents.Add<App>("app");
-var app = builder.Build();
-app.MainWindow.SetTitle("DnSpyXDX").SetIconFile(Path.Combine(AppContext.BaseDirectory, "wwwroot", "dnspyxdx.png")).SetSize(1320, 840).SetMinSize(860, 560).SetUseOsDefaultSize(false);
-WindowStateManager.Attach(app.MainWindow);
-app.Run();
+internal static class Program
+{
+    // WebView2 can only be created from an STA thread
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+        builder.Services.AddLogging(logging => logging.AddConsole());
+        builder.Services.AddSingleton<IDecompilerBackend, DecompilerBackend>();
+        builder.Services.AddSingleton<IProjectExportService, ProjectExportService>();
+        builder.Services.AddSingleton<WorkspaceState>();
+        builder.Services.AddSingleton<IFileDialogService, PhotinoFileDialogService>();
+        builder.Services.AddSingleton<IWorkspaceSessionService, WorkspaceSessionService>();
+        builder.RootComponents.Add<App>("app");
+        var app = builder.Build();
+        app.MainWindow.SetTitle("DnSpyXDX").SetIconFile(Path.Combine(AppContext.BaseDirectory, "wwwroot", "dnspyxdx.png")).SetSize(1320, 840).SetMinSize(860, 560).SetUseOsDefaultSize(false);
+        WindowStateManager.Attach(app.MainWindow);
+        app.Run();
+    }
+}
